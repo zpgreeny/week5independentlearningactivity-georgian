12a82349ad6af372fb10ed3860423324417feee4
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-02-05 15:01:18 -0500

    Add files via upload

---
+++ comp1073.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+//add your object here 
+const jess = {
+  name: ['Jessica','Gilfillan'],
+  age:34,
+  interests: ['coffee', 'code', 'cats'],
+  bio: function() {
+    alert(this.name[0] + ' ' + this.name[1]  + ' is ' + this.age + ' years old. They like ' + this.interests[0] + ', ' + this.interests[1] +  ' and ' + this.interests[2] + '.');
+  },
+  greeting: function() {
+    alert('Hi! I\'m ' + this.name[0] + this.age + '.');
+  }
+}
+
+
+//add event listener function or event handler property here 
+
+let button = document.getElementById('jess'); 
+let button1 = document.getElementById('shadow');
+//button.addEventListener('click', jess.greeting); 
+
+button.addEventListener('click', function(){
+  jess.bio(); 
+})
